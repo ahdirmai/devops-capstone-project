f3dbb7dc510f624a1acacd6e443bbf62322e02f9
ahdirmai <ridhofahmij225@gmail.com>
2023-03-02 02:07:16 -0500

    update

--- service/routes.py
+++ service/routes.py
@@ -108,7 +108,7 @@ def update_accounts(account_id):
     account.deserialize(request.get_json())
     account.update()
     return account.serialize(), status.HTTP_200_OK
-
+    
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
@@ -129,7 +129,6 @@ def delete_accounts(account_id):
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################
-
 
 def check_content_type(media_type):
     """Checks that the media type is correct"""
